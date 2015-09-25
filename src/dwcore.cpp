1a1e1df35257ffe7e873ccb66660d53f96008ba9
Richard Schubert <rs@directx9.de>
2015-09-25 16:44:09 +0200

    added missing file

---
+++ src/dwcore.cpp
@@ -0,0 +1,53 @@
+
+#include "dwcore.h"
+
+#include <cpplinq.hpp>
+#include <map>
+
+using namespace std;
+using namespace cpplinq;
+
+namespace dw
+{
+	map<astring, ContentType> ContentTypeIdMapping;
+
+	struct InitStaticCore
+	{
+		InitStaticCore()
+		{
+			for (int ctIndex = 0; ctIndex < CT_NumContentTypes; ctIndex++)
+			{
+				ContentTypeIdMapping[ContentTypeId[ctIndex]] = (ContentType)ctIndex;
+			}
+		}
+
+	} staticRunCtor;
+
+	ContentType GetContentType(const astring& contentTypeId)
+	{
+		auto availableContentType = ContentTypeIdMapping.find(contentTypeId);
+		if (availableContentType == ContentTypeIdMapping.end())
+		{
+			return CT_Unknown;
+		}
+		return availableContentType->second;
+	}
+
+	DataType FindCompatibleDataType(ContentType contentType, const vector<DataType>& availableDataTypes)
+	{
+		switch (contentType)
+		{
+		case CT_Image_PNG:
+			return from(availableDataTypes) >> first_or_default([](DataType dt) { return dt == DT_RGBA8 || dt == DT_U8 || dt == DT_U32; });
+		case CT_Image_Raw_S16:
+		case CT_Image_Elevation:
+			return from(availableDataTypes) >> first_or_default([](DataType dt) { return dt == DT_S16; });
+		case CT_Image_Raw_U8:
+			return from(availableDataTypes) >> first_or_default([](DataType dt) { return dt == DT_U8; });
+		case CT_Image_Raw_U32:
+			return from(availableDataTypes) >> first_or_default([](DataType dt) { return dt == DT_U32; });
+		default:
+			return DT_Unknown;
+		}
+	}
+}
