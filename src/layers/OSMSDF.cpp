61fa03ddf815630b96f6ad949fb8d2a08710cc8d
Richard Schubert <rs@directx9.de>
2016-01-25 13:39:57 +0100

    moved SDF code from TestSDFRasterizer into OSMSDF Layer while keeping the test functional

--- src/layers/OSMSDF.cpp
+++ src/layers/OSMSDF.cpp
@@ -12,6 +12,9 @@
 #pragma warning (push)
 #pragma warning (disable:4251)
 #include <gdal_priv.h>
+#include <ogr_api.h>
+#include <ogr_geometry.h>
+#include <ogrsf_frmts.h>
 #pragma warning (pop)
 #include <cpl_vsi.h>
 
@@ -23,13 +26,115 @@
 #include "../utils/ImageProcessor.h"
 #include "../utils/Filesystem.h"
 
+#include <ogr_spatialref.h>
+#include <LooseQuadtree.h>
+
 using namespace std;
 using namespace std::chrono;
 
 using namespace ZFXMath;
 
+using namespace loose_quadtree;
 using namespace dw;
 using namespace dw::utils;
+
+
+class Polygon
+{
+
+public:
+	GIntBig id;
+	TPolygon2D<double>* polygons;
+	int numPolygons;
+	BoundingBox<double> aabb;
+
+	Polygon(OGRFeature* src)
+		: id(src->GetFID())
+		, polygons(NULL)
+		, numPolygons(0)
+		, aabb(0, 0, 0, 0)
+	{
+		auto poly = (OGRPolygon*)src->GetGeometryRef();
+
+		// get AABB
+		{
+			OGREnvelope env;
+			poly->getEnvelope(&env);
+
+			aabb.left = env.MinX;
+			aabb.top = env.MinY;
+			aabb.width = env.MaxX - env.MinX;
+			aabb.height = env.MaxY - env.MinY;
+		}
+
+		// create outer perimeter
+		{
+			numPolygons = poly->getNumInteriorRings() + 1;
+			polygons = new TPolygon2D<double>[numPolygons];
+
+			auto ring = poly->getExteriorRing();
+			polygons[0].SetNumVertices(ring->getNumPoints());
+			auto vertices = polygons[0].GetVertices();
+			ring->getPoints(&vertices[0].x, sizeof(TVector2D<double>), &vertices[0].y, sizeof(TVector2D<double>));
+			polygons[0].CloseRing();
+		}
+
+		// create inner holes
+		{
+			for (int p = 0; p < poly->getNumInteriorRings(); p++)
+			{
+				auto ring = poly->getInteriorRing(p);
+				polygons[p + 1].SetNumVertices(ring->getNumPoints());
+				auto vertices = polygons[p + 1].GetVertices();
+				ring->getPoints(&vertices[0].x, sizeof(TVector2D<double>), &vertices[0].y, sizeof(TVector2D<double>));
+				polygons[p + 1].CloseRing();
+			}
+		}
+	}
+
+	double ComputeSqrDistanceToBoundingBox(double x, double y) const
+	{
+		const double clampedX = min(max(x, aabb.left), aabb.left + aabb.width);
+		const double clampedY = min(max(y, aabb.top), aabb.top + aabb.height);
+
+		const double deltaX = clampedX - x;
+		const double deltaY = clampedY - y;
+
+		return deltaX * deltaX + deltaY * deltaY;
+	}
+
+	double ComputeSignedSquareDistance(double x, double y) const
+	{
+		bool pointIsRightOfEdge = false;
+		TVector2D<double> point(x, y);
+		double sqrDistance = polygons[0].ComputeSqrDistance(point, pointIsRightOfEdge);
+
+		for (int p = 1; p < numPolygons; p++)
+		{
+			bool pointIsRightOfInnerEdge = false;
+			double innerSqrDistance = polygons[p].ComputeSqrDistance(point, pointIsRightOfInnerEdge);
+			if (innerSqrDistance < sqrDistance)
+			{
+				sqrDistance = innerSqrDistance;
+				pointIsRightOfEdge = pointIsRightOfInnerEdge;
+			}
+		}
+
+		return pointIsRightOfEdge ? -sqrDistance : sqrDistance;
+	}
+};
+
+class BoundingBoxExtractor
+{
+public:
+	static void ExtractBoundingBox(Polygon* poly, BoundingBox<double>* bb)
+	{
+		*bb = poly->aabb;
+	}
+};
+
+typedef LooseQuadtree<double, Polygon, BoundingBoxExtractor> LooseGeoQuadtree;
+void SplitPolygons(const char* source_name, const char* dest_name);
 
 namespace dw
 {
@@ -42,6 +147,9 @@ namespace dw
 		class OSMSDF : public WebMapService::Layer
 		{
 			OGRSpatialReference* OSM_SpatRef;
+
+			LooseGeoQuadtree qt;
+			vector<Polygon*> landPolygons;
 		public:
 
 			virtual bool Init(/* layerconfig */) override
@@ -53,6 +161,16 @@ namespace dw
 					cout << "OSMSDF: " << "CRS support for EPSG:4326 is mandatory" << endl;
 					return false;
 				}
+
+				const char* coastlineShapeFileSrc = "../../test/coastline/land_polygons.shp";
+				const char* coastlineShapeFileDst = "../../test/coastline/land_polygons_split.shp";
+
+				if (!exists(coastlineShapeFileDst))
+				{
+					SplitPolygons(coastlineShapeFileSrc, coastlineShapeFileDst);
+				}
+
+				if (!LoadLandShapes(coastlineShapeFileDst)) return false;
 				
 				return true;
 			}
@@ -101,6 +219,12 @@ namespace dw
 
 			virtual ~OSMSDF() override
 			{
+				const size_t numShapes = landPolygons.size();
+				for (size_t n = 0; n < numShapes; n++)
+				{
+					delete landPolygons[n];
+				}
+				landPolygons.clear();
 			}
 
 			HandleGetMapRequestResult HandleGetMapRequest(const WebMapService::GetMapRequest& gmr, const OGRSpatialReference* requestSRS, Image& img)
@@ -113,23 +237,137 @@ namespace dw
 					return HGMRR_InvalidBBox; // TODO: according to WMS specs bbox may lay outside of valid areas (e.g. latitudes greater than 90 degrees in CRS:84)
 				}
 
-				HandleGetMapRequestResult result = HGMRR_OK;
-
 				high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
-				// TODO: render to image
 				memset(img.rawData, 0x7F, img.rawDataSize);
+				HandleGetMapRequestResult result = RasterImg(gmr, requestSRS, img);
 
 				high_resolution_clock::time_point t2 = high_resolution_clock::now();
 				duration<double> time_span = duration_cast<duration<double>>(t2 - t1) * 1000.0;
 				std::cout << "OSM SDF computation took " << std::setprecision(5) << time_span.count() << " ms" << endl;
 
-				if (false) // debug output of loaded region of ASTER tiles
+				return result;
+			}
+
+			HandleGetMapRequestResult RasterImg(const WebMapService::GetMapRequest& gmr, const OGRSpatialReference* requestSRS, Image& img)
+			{
+				const int Width = img.width;
+				const int Height = img.height;
+
+				const double DistanceDomain = 10.0;
+				const double cellSize = 360.0 / Width;
+				const double ScaledDistanceDomain = cellSize * DistanceDomain;
+
+				//omp_lock_t myLock;
+				//omp_init_lock(&myLock);
+				atomic_int yProgress = 0;
+
+				//#pragma omp parallel for
+				for (int y = 0; y < Height; y++)
 				{
-					img.SaveToPNG("osmsdf.png");
+					u8* world = &img.rawData[(Height - y - 1) * Width];
+
+					for (int x = 0; x < Width; x++)
+					{
+						*world = 0;
+
+						BoundingBox<double> bb(
+							x * cellSize - 180.0 - ScaledDistanceDomain,
+							y * cellSize - 90.0 - ScaledDistanceDomain, cellSize + ScaledDistanceDomain * 2.0, cellSize + ScaledDistanceDomain * 2.0);
+						double minSqrDistance = numeric_limits<double>::max();
+
+						double px = bb.left + bb.width * 0.5;
+						double py = bb.top + bb.height * 0.5;
+
+						//omp_set_lock(&myLock);
+						LooseGeoQuadtree::Query query = qt.QueryIntersectsRegion(bb);
+						//omp_unset_lock(&myLock);
+
+						while (!query.EndOfQuery())
+						{
+							Polygon* poly = query.GetCurrent();
+
+							const double aabbDistanceSqr = poly->ComputeSqrDistanceToBoundingBox(px, py);
+							if (aabbDistanceSqr < minSqrDistance) // test distance to AABB first before doing the expensive polygon distance test
+							{
+								double sqrDistance = poly->ComputeSignedSquareDistance(px, py);
+								minSqrDistance = min(minSqrDistance, sqrDistance);
+							}
+
+							if (minSqrDistance < 0.0)
+							{
+								break;
+							}
+
+							query.Next();
+						}
+						while (!query.EndOfQuery()) // we have to finish a query to release it from the quadtree
+						{
+							query.Next();
+						}
+
+						if (minSqrDistance == numeric_limits<double>::max())
+						{
+							(*world) = 255;
+						}
+						else
+						{
+							double minDistance = Sign(minSqrDistance) * Sqrt(Abs(minSqrDistance));
+							u8 worldValue = (u8)Clamp(255.0 * (minDistance * 0.5 / ScaledDistanceDomain + 0.5), 0.0, 255.0);
+							(*world) = worldValue;
+						}
+
+						world++;
+					}
+
+					yProgress++;
+					cout << "rasterized: " << (int)(100.0f * yProgress / (float)Height) << "%s\r";
 				}
 
-				return result;
+				return HGMRR_OK;
+			}
+
+			bool LoadLandShapes(const char* landShapeFile)
+			{
+				RegisterOGRShape();
+				auto driver = OGRGetDriverByName("ESRI Shapefile");
+				OGRDataSource* source = (OGRDataSource*)OGROpen(landShapeFile, GA_ReadOnly, &driver);
+				if (!source)
+				{
+					cout << "OSMSDF: " << "Unable to open shape file " << landShapeFile << endl;
+					return false;
+				}
+
+				OGRLayer* srcLayer = source->GetLayer(0);
+				GIntBig numFeatures = srcLayer->GetFeatureCount();
+				srcLayer->ResetReading();
+
+				landPolygons.resize(numFeatures);
+
+				GIntBig featureIndex = 0;
+				OGRFeature* feature;
+				while ((feature = srcLayer->GetNextFeature()) != NULL)
+				{
+					OGRGeometry* geometry = feature->GetGeometryRef();
+
+					if (featureIndex % 10000 == 0)
+					{
+						cout << "OSMSDF: " << "shapes loaded: " << featureIndex << " / " << numFeatures << "\r";
+					}
+
+					auto poly = new Polygon(feature);
+
+					qt.Insert(poly);
+					landPolygons[featureIndex] = poly;
+
+					OGRFeature::DestroyFeature(feature);
+
+					featureIndex++;
+				}
+
+				OGRDataSource::DestroyDataSource(source);
+
+				return true;
 			}
 		};
 
--- test/TestSDFRasterizer.cpp
+++ test/TestSDFRasterizer.cpp
@@ -7,7 +7,6 @@
 #include <vector>
 #include <algorithm>
 #include <ogr_spatialref.h>
-#include <LooseQuadtree.h>
 #include <omp.h>
 #include <atomic>
 
@@ -20,112 +19,16 @@
 #pragma warning (pop)
 
 #include <../src/dwcore.h>
+#include <../src/WebMapService.h>
 #include <../src/utils/Filesystem.h>
+#include <../src/utils/ImageProcessor.h>
 
 using namespace ZFXMath;
 using namespace std;
 using namespace std::chrono;
-using namespace loose_quadtree;
 using namespace dw;
 
 #define TestTag "TestSDFRasterizer - "
-
-
-class Polygon
-{
-
-public:
-	GIntBig id;
-	TPolygon2D<double>* polygons;
-	int numPolygons;
-	BoundingBox<double> aabb;
-
-	Polygon(OGRFeature* src)
-		: id(src->GetFID())
-		, polygons(NULL)
-		, numPolygons(0)
-		, aabb(0, 0, 0, 0)
-	{
-		auto poly = (OGRPolygon*)src->GetGeometryRef();
-
-		// get AABB
-		{
-			OGREnvelope env;
-			poly->getEnvelope(&env);
-
-			aabb.left = env.MinX;
-			aabb.top = env.MinY;
-			aabb.width = env.MaxX - env.MinX;
-			aabb.height = env.MaxY - env.MinY;
-		}
-
-		// create outer perimeter
-		{
-			numPolygons = poly->getNumInteriorRings() + 1;
-			polygons = new TPolygon2D<double>[numPolygons];
-
-			auto ring = poly->getExteriorRing();
-			polygons[0].SetNumVertices(ring->getNumPoints());
-			auto vertices = polygons[0].GetVertices();
-			ring->getPoints(&vertices[0].x, sizeof(TVector2D<double>), &vertices[0].y, sizeof(TVector2D<double>));
-			polygons[0].CloseRing();
-		}
-
-		// create inner holes
-		{
-			for (int p = 0; p < poly->getNumInteriorRings(); p++)
-			{
-				auto ring = poly->getInteriorRing(p);
-				polygons[p + 1].SetNumVertices(ring->getNumPoints());
-				auto vertices = polygons[p + 1].GetVertices();
-				ring->getPoints(&vertices[0].x, sizeof(TVector2D<double>), &vertices[0].y, sizeof(TVector2D<double>));
-				polygons[p + 1].CloseRing();
-			}
-		}
-	}
-
-	double ComputeSqrDistanceToBoundingBox(double x, double y) const 
-	{
-		const double clampedX = min(max(x, aabb.left), aabb.left + aabb.width);
-		const double clampedY = min(max(y, aabb.top), aabb.top + aabb.height);
-
-		const double deltaX = clampedX - x;
-		const double deltaY = clampedY - y;
-
-		return deltaX * deltaX + deltaY * deltaY;
-	}
-
-	double ComputeSignedSquareDistance(double x, double y) const
-	{
-		bool pointIsRightOfEdge = false;
-		TVector2D<double> point(x, y);
-		double sqrDistance = polygons[0].ComputeSqrDistance(point, pointIsRightOfEdge);
-
-		for (int p = 1; p < numPolygons; p++)
-		{
-			bool pointIsRightOfInnerEdge = false;
-			double innerSqrDistance = polygons[p].ComputeSqrDistance(point, pointIsRightOfInnerEdge);
-			if (innerSqrDistance < sqrDistance)
-			{
-				sqrDistance = innerSqrDistance;
-				pointIsRightOfEdge = pointIsRightOfInnerEdge;
-			}
-		}
-
-		return pointIsRightOfEdge ? -sqrDistance : sqrDistance;		
-	}
-};
-
-class BoundingBoxExtractor
-{
-public:
-	static void ExtractBoundingBox(Polygon* poly, BoundingBox<double>* bb)
-	{
-		*bb = poly->aabb;
-	}
-};
-
-
 
 //#pragma optimize( "", off )
 void SplitPolygons(const char* source_name, const char* dest_name);
@@ -133,152 +36,31 @@ bool TestSDFRasterizer()
 {
 	GDALRegister_GTiff();
 
-	const char* coastlineShapeFileSrc = "../../test/coastline/land_polygons.shp";
-	const char* coastlineShapeFileDst = "../../test/coastline/land_polygons_split.shp";
-
-	if (!exists(coastlineShapeFileDst))
-	{
-		SplitPolygons(coastlineShapeFileSrc, coastlineShapeFileDst);
-	}
-
-	typedef LooseQuadtree<double, Polygon, BoundingBoxExtractor> LooseGeoQuadtree;
-	LooseGeoQuadtree qt;
-	vector<Polygon*> polygons;
-
-	// load shape file
-	{
-		RegisterOGRShape();
-		auto driver = OGRGetDriverByName("ESRI Shapefile");
-		OGRDataSource* source = (OGRDataSource*)OGROpen(coastlineShapeFileDst, GA_ReadOnly, &driver);
-		if (!source)
-		{
-			printf(TestTag "Unable to open shape file\n");
-			return false;
-		}
-
-		OGRLayer* srcLayer = source->GetLayer(0);
-		GIntBig numFeatures = srcLayer->GetFeatureCount();
-		srcLayer->ResetReading();
-
-		//auto epsg4326 = (OGRSpatialReference*)OSRNewSpatialReference(NULL);
-		//auto epsg3857 = (OGRSpatialReference*)OSRNewSpatialReference(NULL);
-		//epsg4326->importFromEPSG(4326);
-		//epsg3857->importFromEPSG(3857);
-		//OGRCoordinateTransformation* transform = OGRCreateCoordinateTransformation(epsg3857, epsg4326);
-
-		polygons.reserve(numFeatures);
-
-		GIntBig featureIndex = 0;
-		OGRFeature* feature;
-		while ((feature = srcLayer->GetNextFeature()) != NULL) 
-		{
-			OGRGeometry* geometry = feature->GetGeometryRef();
-		
-			/*OGRErr error = geometry->transform(transform);
-			if (error != OGRERR_NONE)
-			{
-				printf(TestTag "Shape Coordinate Transform failed\n");
-				return false;
-			}*/
-
-			if (featureIndex % 10000 == 0)
-			{
-				std::cerr << "shapes loaded: " << featureIndex << " / " << numFeatures << "\r";
-			}
-
-			auto poly = new Polygon(feature);
-
-			qt.Insert(poly);
-			polygons[featureIndex] = poly;
-
-			OGRFeature::DestroyFeature(feature);
-
-			featureIndex++;
-		}
-
-		//OGRCoordinateTransformation::DestroyCT(transform);
-		//OGRSpatialReference::DestroySpatialReference(epsg3857);
-		//OGRSpatialReference::DestroySpatialReference(epsg4326);
-
-		OGRDataSource::DestroyDataSource(source);
-	}
-
 	const int Width = 3600;
 	const int Height = 1800;
-	u8* worldImg = new u8[Width * Height];
+	Image img(Width, Height, DT_U8);
 
-	// raster image
-	{
-		const double DistanceDomain = 10.0;
-		const double cellSize = 360.0 / Width;
-		const double ScaledDistanceDomain = cellSize * DistanceDomain;
+	map<std::string, WebMapService::Layer*> layers;
+	layers["OSM_SDF"] = NULL;
+	WebMapService::LayerFactory::CreateLayers(layers);
 
-		omp_lock_t myLock;
-		omp_init_lock(&myLock);
-		atomic_int yProgress = 0;
-
-		#pragma omp parallel for
-		for (int y = 0; y < Height; y++)
-		{
-			u8* world = &worldImg[(Height - y - 1) * Width];
-
-			for (int x = 0; x < Width; x++)
-			{
-				*world = 0;
-
-				BoundingBox<double> bb(
-					x * cellSize - 180.0 - ScaledDistanceDomain,
-					y * cellSize - 90.0 - ScaledDistanceDomain, cellSize + ScaledDistanceDomain * 2.0, cellSize + ScaledDistanceDomain * 2.0);
-				double minSqrDistance = numeric_limits<double>::max();
-
-				double px = bb.left + bb.width * 0.5;
-				double py = bb.top + bb.height * 0.5;
-
-				omp_set_lock(&myLock);
-				LooseGeoQuadtree::Query query = qt.QueryIntersectsRegion(bb);
-				omp_unset_lock(&myLock);
-
-				while (!query.EndOfQuery())
-				{
-					Polygon* poly = query.GetCurrent();
-
-					const double aabbDistanceSqr = poly->ComputeSqrDistanceToBoundingBox(px, py);
-					if (aabbDistanceSqr < minSqrDistance) // test distance to AABB first before doing the expensive polygon distance test
-					{
-						double sqrDistance = poly->ComputeSignedSquareDistance(px, py);
-						minSqrDistance = min(minSqrDistance, sqrDistance);
-					}
-
-					if (minSqrDistance < 0.0)
-					{
-						break;
-					}
-
-					query.Next();
-				}
-
-				if (minSqrDistance == numeric_limits<double>::max())
-				{
-					(*world) = 255;
-				}
-				else
-				{
-					double minDistance = Sign(minSqrDistance) * Sqrt(Abs(minSqrDistance));
-					u8 worldValue = (u8)Clamp(255.0 * (minDistance * 0.5 / ScaledDistanceDomain + 0.5), 0.0, 255.0);
-					(*world) = worldValue;
-				}
-
-				world++;
-			}
-
-			yProgress++;
-			std::cerr << "rasterized: " << (int)(100.0f * yProgress / (float)Height) << "%s\r";
-		}
-	}
+	auto osmSDFLayer = layers["OSM_SDF"];
 
 	// write file to disk
 	{
-		u8* world = worldImg;
+		u8* world = img.rawData;
+
+		WebMapService::GetMapRequest gmr;
+		gmr.styles = "";
+		gmr.crs = "EPSG:4326";
+		gmr.bbox.minX = -180.0;
+		gmr.bbox.maxX = -180.0;
+		gmr.bbox.minY = -90.0;
+		gmr.bbox.maxY = -90.0;
+		gmr.width = Width;
+		gmr.height = Height;
+		gmr.dataType = DT_U8;
+		osmSDFLayer->HandleGetMapRequest(gmr, img);
 
 		auto gtiffDriver = GetGDALDriverManager()->GetDriverByName("GTiff");
 		auto dataSet = gtiffDriver->Create("C:/dev/landWaterMask.tiff", Width, Height, 1, GDT_Byte, NULL);
@@ -293,15 +75,6 @@ bool TestSDFRasterizer()
 		}
 		GDALClose(dataSet);
 	}
-
-	delete[] worldImg;
-
-	const size_t numShapes = polygons.size();
-	for (size_t n = 0; n < numShapes; n++)
-	{
-		delete polygons[n];
-	}
-	polygons.clear();
 
 	return true;
 }
